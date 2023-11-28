2d66118fa787ea2f64a763a4beb3cd6417522846
c0b22175e4 <c0b22175e4@edu.teu.ac.jp>
2023-11-28 14:55:22 +0900

    練習2：爆弾Surfaceを移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,23 +1,20 @@
 import random
 import sys
 import pygame as pg
-
-
 WIDTH, HEIGHT = 1600, 900
-
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20, 20))  # 練習1：透明なSurfaveを作る
-    bb_img.set_colorkey((0, 0, 0))  # 練習1：黒い部分を透明にする
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習1：赤い半径10の円
-    bb_rect = bb_img.get_rect()  #  練習2：爆弾SuefaceのRectを抽出する
-    bb_rect.centerx = random.randint(0, WIDTH)
-    bb_rect.centery = random.randint(0, HEIGHT)
+    bb_img = pg.Surface((20, 20))   # 練習１：透明のSurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習１：赤い半径10の円を描く
+    bb_rct = bb_img.get_rect()  # 練習１：爆弾SurfaceのRectを抽出する
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 練習２：爆弾の速度
 
     clock = pg.time.Clock()
     tmr = 0
@@ -28,10 +25,12 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, bb_rect)
+        bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
